d005daa525774255c9844f3be6852ac463ebaa07
Tim S <stahta01@users.sourceforge.net>
2015-10-04 11:03:02 -0400

    Commented out code that prevents Windows users from changing shell.

--- src/src/environmentsettingsdlg.cpp
+++ src/src/environmentsettingsdlg.cpp
@@ -133,10 +133,10 @@ EnvironmentSettingsDlg::EnvironmentSettingsDlg(wxWindow* parent, wxAuiDockArt* a
 
     wxTextCtrl* txt = XRCCTRL(*this, "txtConsoleShell", wxTextCtrl);
     txt->SetValue(cfg->Read(_T("/console_shell"), DEFAULT_CONSOLE_SHELL));
-#ifdef __WXMSW__
-    // under win32, this option is not needed, so disable it
-    txt->Enable(false);
-#endif
+//#ifdef __WXMSW__
+//    // under win32, this option is not needed, so disable it
+//    txt->Enable(false);
+//#endif
 
     wxComboBox *combo = XRCCTRL(*this, "cbConsoleTerm", wxComboBox);
     combo->Append(DEFAULT_CONSOLE_TERM);
