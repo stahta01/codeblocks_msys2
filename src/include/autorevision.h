16831bab4760252fad3fb681752ae3b79afb29c4
Tim S <stahta01@users.sourceforge.net>
2016-06-04 15:59:48 -0400

    + sdk: Added "autorevision.h".

---
+++ src/include/autorevision.h
@@ -0,0 +1,18 @@
+/*10693*/
+//don't include this header, only configmanager-revision.cpp should do this.
+#ifndef AUTOREVISION_H
+#define AUTOREVISION_H
+
+
+#include <wx/string.h>
+
+namespace autorevision
+{
+	const unsigned int svn_revision = 10693;
+	const wxString svnRevision(_T("10693"));
+	const wxString svnDate(_T("2016-01-23 11:26:04"));
+}
+
+
+
+#endif
